8f03e5983bb3ebe725b2c5d70263b7627bc57c90
ali <aliismaildeveloper@gmail.com>
2024-12-14 22:08:44 -0800

    Player movement successfuly

--- game/player.py
+++ game/player.py
@@ -1,6 +1,15 @@
 class Player:
-    def __init__(self, start_position):
+    def __init__(self, start_position, maze):
         self.position = start_position
+        self.maze = maze
 
     def move(self, direction):
-        pass
+        x, y = self.position
+        if direction == "up" and self.maze[x - 1][y] == 1:
+            self.position = (x - 1, y)
+        elif direction == "down" and self.maze[x + 1][y] == 1:
+            self.position = (x + 1, y)
+        elif direction == "left" and self.maze[x][y - 1] == 1:
+            self.position = (x, y - 1)
+        elif direction == "right" and self.maze[x][y + 1] == 1:
+            self.position = (x, y + 1)
